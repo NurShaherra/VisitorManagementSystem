e2e2739e00a17d091f95ea4606d47d1676f728df
15031777 <15031777@myrp.edu.sg>
2017-05-17 10:19:31 +0800

    Created 3 new activity for security guard, manager, host. Created 2 new dbHandler class for visitor and records.

--- app/src/main/java/com/example/a15031777/visitormanagementsystem/HostActivity.java
+++ app/src/main/java/com/example/a15031777/visitormanagementsystem/HostActivity.java
@@ -3,7 +3,7 @@ package com.example.a15031777.visitormanagementsystem;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class VisitorActivity extends AppCompatActivity {
+public class HostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
