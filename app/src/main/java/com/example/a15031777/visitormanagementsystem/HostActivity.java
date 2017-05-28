934cd1fd854d9c4e018f9bac31fe60c570068ddc
actuallycoding <15017484@myrp.edu.sg>
2017-05-28 21:26:51 +0800

    Remove duplicate Host Activities.

--- app/src/main/java/com/example/a15031777/visitormanagementsystem/HostActivity.java
+++ app/src/main/java/com/example/a15031777/visitormanagementsystem/HostActivity.java
@@ -8,6 +8,6 @@ public class HostActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_visitor);
+        setContentView(R.layout.activity_host_register);
     }
 }
